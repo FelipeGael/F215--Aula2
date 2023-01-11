9d70df6f93d3bbd936e818c2519833c408939973
ronald-silva <ronaldsilvajob@yahoo.com>
2023-01-11 10:01:05 -0300

    calculadora

---
+++ scripts.js
@@ -0,0 +1,34 @@
+//document.getElementyById("container").innerHTML ="conteudo";
+
+let container = document.getElementById("container");
+
+//let maças = 2;
+//let bananas = 3;
+//let soma = maças + bananas;
+
+//container.innerHTML = soma;
+
+
+//let texto = "meu nome é ";
+//let nome = "felipe ferreira";
+//let soma = texto + nome;
+
+//container.innerHTML = soma;
+
+//const NASCIMENTO = "03/10/2000";
+//let nome = "felipe ferreira";
+
+//container.innerHTML = nome + NASCIMENTO;
+
+
+function somar()
+{
+  let v1 = Number( valor1.value);
+  let v2 = Number(valor2.value);
+  document.getElementById("resultado").innerHTML = v1 + v2;
+
+
+}
+
+
+
